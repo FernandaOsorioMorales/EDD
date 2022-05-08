fa2e4c9c622ca7cf02f21cee6cf38b807ed13ccc
Fernanda <ferame.om@gmail.com>
2022-05-08 16:55:46 -0500

    Correciones AVL

--- ArbolAVL.java
+++ ArbolAVL.java
@@ -45,7 +45,7 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
         }
 
         /**
-         * Método que devuelva la altura del árbol AVL
+         * Método que devuelva la altura del vértice AVL
          */
         public int getAltura() {
             if (this == null) {
@@ -62,7 +62,8 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
             if (this == null) {
                 return 0;
             }
-            return Math.abs(this.izquierdo.getAltura() - this.derecho.getAltura());
+            VerticeAVL raiz = convertirAVL(this);
+            return Math.abs(raiz.getAltura() - raiz.getAltura());
         }
 
         /**
@@ -87,7 +88,8 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
      */
     public void add(T elemento) {
         insert(elemento);
-        rebalancear(this.raiz);
+        VerticeAVL verticeInsertado = new VerticeAVL(elemento);
+        rebalancear(verticeInsertado);
     }
 
     /**
@@ -95,7 +97,18 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
      */
     public void delete(T elemento) {
         delete(raiz, elemento);
-        rebalancear(this.raiz);
+        VerticeAVL VerticeEliminado = new VerticeAVL(elemento);
+        VerticeAVL padreVerticeEliminado = new VerticeAVL(VerticeEliminado.padre.elemento);
+        rebalancear(padreVerticeEliminado);
+    }
+
+    /**
+     * Método que convierte un vértice del árbol binario a un vértice AVL
+     * 
+     * @param vertice
+     */
+    private VerticeAVL convertirAVL(Vertice vertice) {
+        return (VerticeAVL) vertice;
     }
 
     /**
@@ -106,20 +119,20 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
     private void rebalancear(VerticeAVL vertice) {
         // primero vemos si esta desbalanceado hacia la derecha
         // guardamos en una variable la altura del subarbol derecho e izquierdo
-        int alturaHijoDerecho = vertice.derecho.getAltura();
-        int alturaHijoIzquierdo = vertice.izquierdo.getAltura();
-        VerticeAVL hijoDerecho = vertice.derecho;
-        VerticeAVL hijoIzquierdo = vertice.derecho;
+        VerticeAVL hijoDerecho = convertirAVL(vertice.derecho);
+        VerticeAVL hijoIzquierdo = convertirAVL(vertice.izquierdo);
+        int alturaHijoDerecho = hijoDerecho.getAltura();
+        int alturaHijoIzquierdo = hijoIzquierdo.getAltura();
 
         // si esta desbalanceado hacia la derecha
-        if (alturaHijoDerecho = alturaHijoIzquierdo + 2) {
+        if (alturaHijoDerecho == alturaHijoIzquierdo + 2) {
             // si la altura del hijo derecho del hijo derecho es k+1
-            VerticeAVL nietoDerechoD = hijoDerecho.derecho;
-            VerticeAVL nietoIzquierdoD = hijoDerecho.izquierdo;
+            VerticeAVL nietoDerechoD = convertirAVL(hijoDerecho.derecho);
+            VerticeAVL nietoIzquierdoD = convertirAVL(hijoDerecho.izquierdo);
             int alturaNietoDerechoD = nietoDerechoD.getAltura();
             // si la altura del nieto derecho es igual a la altura del hijo izquierdo más
             // uno
-            if (alturaNietoDerechoD = alturaHijoIzquierdo + 1) {
+            if (alturaNietoDerechoD == alturaHijoIzquierdo + 1) {
                 hijoDerecho.izquierdo = vertice;
                 vertice.padre = hijoDerecho;
                 vertice.izquierdo = hijoIzquierdo;
@@ -127,7 +140,7 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
 
             }
             // si la altura del nieto derecho es igual a la altura del hijo izquierdo
-            if (alturaNietoDerechoD = alturaHijoIzquierdo) {
+            if (alturaNietoDerechoD == alturaHijoIzquierdo) {
                 vertice.derecho = nietoIzquierdoD;
                 nietoIzquierdoD.derecho = hijoDerecho;
                 hijoDerecho.derecho = nietoDerechoD;
@@ -140,20 +153,20 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
         }
 
         // si esta desbalanceado hacia la izquierda
-        if (alturaHijoIzquierdo = alturaHijoDerecho + 2) {
-            VerticeAVL nietoDerechoI = hijoIzquierdo.derecho;
-            VerticeAVL nietoIzquierdoI = hijoIzquierdo.izquierdo;
+        if (alturaHijoIzquierdo == alturaHijoDerecho + 2) {
+            VerticeAVL nietoDerechoI = convertirAVL(hijoIzquierdo.derecho);
+            VerticeAVL nietoIzquierdoI = convertirAVL(hijoIzquierdo.izquierdo);
             int alturaNietoIzquierdoI = nietoIzquierdoI.getAltura();
             // si la altura del nieto izquierdo es igual a la altura del hijo derecho más
             // uno
-            if (alturaNietoIzquierdoI = alturaHijoDerecho + 1) {
+            if (alturaNietoIzquierdoI == alturaHijoDerecho + 1) {
                 hijoIzquierdo.derecho = vertice;
                 vertice.padre = hijoIzquierdo;
                 vertice.derecho = hijoDerecho;
                 vertice.izquierdo = nietoDerechoI;
             }
             // si la altura del nieto izquierdo es igual a la altura del hijo derecho
-            if (alturaNietoIzquierdoI = alturaHijoDerecho) {
+            if (alturaNietoIzquierdoI == alturaHijoDerecho) {
                 vertice.izquierdo = nietoDerechoI;
                 nietoDerechoI.padre = vertice;
                 nietoDerechoI.izquierdo = hijoIzquierdo;
@@ -163,6 +176,11 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
                 vertice.derecho = hijoDerecho;
             }
 
+        }
+        VerticeAVL padre = convertirAVL(vertice.padre);
+        rebalancear(padre);
+        if (padre == null) {
+            return;
         }
     }
 
@@ -174,7 +192,6 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
      * @param vertice el vértice sobre el que se quiere girar.
      * @throws UnsupportedOperationException siempre.
      */
-    @Override
     public void giraDerecha(VerticeArbolBinario<T> vertice) {
         throw new UnsupportedOperationException("Los árboles AVL no  pueden " +
                 "girar a la izquierda por el " +
@@ -189,7 +206,6 @@ public class ArbolAVL<T extends Comparable> extends ArbolBinarioBusqueda<T> {
      * @param vertice el vértice sobre el que se quiere girar.
      * @throws UnsupportedOperationException siempre.
      */
-    @Override
     public void giraIzquierda(VerticeArbolBinario<T> vertice) {
         throw new UnsupportedOperationException("Los árboles AVL no  pueden " +
                 "girar a la derecha por el " +
